0b0a07c5168cc79d3b07576a4edd5cf97e42ad6e
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2023-07-02 18:19:43 -0500

    btSlave.cpp

---
+++ bt/btSlave.cpp
@@ -0,0 +1,39 @@
+#include <Arduino.h>
+#include <BluetoothSerial.h>
+bool emparejado = false;
+BluetoothSerial SerialBT;
+long ti = millis();
+void setup()
+{
+  Serial.begin(115200);
+  SerialBT.begin("ESP32_Esclavo");  // Nombre del dispositivo Bluetooth del esclavo
+  SerialBT.connect("ESP32_Master"); // Nombre del dispositivo Bluetooth del maestro
+  Serial.println("ESP32 esclavo iniciado");
+}
+
+void loop()
+{
+  while (!emparejado)
+  {
+    if (!SerialBT.connected())
+    {
+      Serial.println("Esperando emparejamiento...");
+      delay(1000);
+    }
+    else
+    {
+      Serial.println("Emparejado con el maestro");
+      emparejado = true;
+    }
+  }
+
+  if (SerialBT.available())
+  {
+    Serial.print(SerialBT.readString());
+  }
+
+  if (Serial.available())
+  {
+    SerialBT.print(Serial.readString());
+  }
+}
