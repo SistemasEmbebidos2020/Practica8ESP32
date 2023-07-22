1b23bc2ac409a037963f978365bcf5107faf9ef7
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2023-07-22 10:26:10 -0500

    btled.cpp

--- bt/btled.ino
+++ bt/btled.ino
@@ -1,29 +1,49 @@
 #include <Arduino.h>
+#include <BluetoothSerial.h>
+bool emparejado = false;  // Se establece la variable emparejado para saber si se a conectado a un bluetooth
+BluetoothSerial SerialBT; // Se estable una variable SerialBT para poder hacer uso de los comandos
+int led1 = 2;             // Se define la variable led1 con el valor de 2, pin GIOP2 a utilizarse como salida para el led
 
-#include "BluetoothSerial.h"
-
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-#endif
-
-BluetoothSerial SerialBT;
-int led = 2;
-void setup() {
-  Serial.begin(115200);
-  SerialBT.begin("ESP32Embebidos"); //Bluetooth device name
-  Serial.println("The device started, now you can pair it with bluetooth!");
-  pinMode(led,OUTPUT);
+void connect_successful()
+{
+  while (!emparejado) //
+  {
+    if (!SerialBT.connected())
+    {
+      Serial.println("Esperando emparejamiento...");
+      delay(1000);
+    }
+    else
+    {
+      Serial.println("Emparejado con el esclavo");
+      emparejado = true;
+    }
+  }
 }
 
-void loop() {
-  if (Serial.available()) {
-    SerialBT.print(Serial.readString());
+void setup()
+{
+  Serial.begin(115200);
+  SerialBT.begin("ESP32_Esclavo1");         // Nombre del dispositivo Bluetooth del esclavo
+  SerialBT.setPin("bebe");                  // Establecer clave para conexion segura bluetooth
+  Serial.println("ESP32 esclavo iniciado"); // Se Imprime mensaje indicando que el bluetooth esclavo se inició
+  pinMode(led1, OUTPUT);                    // Se establece pin led1=2 como salida
+}
+
+void loop()
+{
+  connect_successful(); // No saldrá de esta función hasta que se conecte correctamente con el dispositivo colocado en SerialBT.connect
+  if (SerialBT.available())
+  {                                   // Se espera a recibir algo por bluetooth
+    String v = SerialBT.readString(); // Se guarda en la variable v, lo recibido por bluetooth
+    Serial.println(v);                // Se imprimer en serial la variable v
+    if (v == "on1")
+    {
+      digitalWrite(led1, HIGH); // se verifica si lo recibido por bluetooth es = a on1 --- para encender led1
+    }
+    else if (v == "off1")
+    {
+      digitalWrite(led1, LOW); // se verifica si lo recibido por bluetooth es = a off1 --- para apagar led1
+    }
   }
-  if (SerialBT.available()) {
-    String v = SerialBT.readString();
-    Serial.println(v);
-    if (v == "on1") digitalWrite(led,HIGH);
-    else if (v == "off1")  digitalWrite(led,LOW);
-  }
-  delay(20);
 }
